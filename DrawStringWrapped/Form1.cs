44344ba9370e4d8477beda126138ae40a04ae970
Kulpot <kulpotwahu@gmail.com>
2023-04-19 05:41:20 +0800

    Add project files.

---
+++ DrawStringWrapped/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawStringWrapped
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Rectangle rect = new Rectangle(10, 10, 200, 200);
+            e.Graphics.FillRectangle(Brushes.SaddleBrown, rect);
+            rect.Inflate(-10, -10);
+            string text = @"A particular Bible passage, sometimes a single verse or verse fragment 
+Textbook, a standardized instructional book 
+in electronic communication and computing";
+
+            var format = new StringFormat(StringFormatFlags.DirectionVertical);
+            e.Graphics.DrawString(text, this.Font, Brushes.Pink, rect, format);
+        }
+    }
+}
